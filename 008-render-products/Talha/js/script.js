758dea7d0073a19ca0d8d4eab4c200e4297e034b
Armstrong104 <ameertalha22@gmail.com>
2025-01-04 02:48:13 +0600

    Use async-await to fetch data

--- 008-render-products/Talha/js/script.js
+++ 008-render-products/Talha/js/script.js
@@ -5,24 +5,34 @@ const loadingComponent = document.getElementById('loading-text');
 const productListComponent = document.getElementById('product-list');
 
 const renderProductsList = (products) => {
-    productListComponent.innerHTML = '';
-    const productListItems = products.map((product) => {
-        const listItem = document.createElement('li');
-        listItem.innerText = product.name;
-        return listItem;
-    });
-    productListComponent.append(...productListItems);
+  productListComponent.innerHTML = '';
+  const productListItems = products.map((product) => {
+    const listItem = document.createElement('li');
+    listItem.innerText = product.name;
+    return listItem;
+  });
+  productListComponent.append(...productListItems);
 };
 
-const renderProducts = () => {
+const renderProducts = async () => {
   loadingComponent.innerText = 'Loading...';
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        renderProductsList(data);
-    })
-    .catch((err) => console.error(err))
-    .finally(() => (loadingComponent.innerText = ''));
+  //   const products = fetch(url)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       renderProductsList(data);
+  //     })
+  //     .catch((err) => console.error(err))
+  //     .finally(() => (loadingComponent.innerText = ''));
+  //   return products;
+  try {
+    const productResponse = await fetch(url);
+    const products = await productResponse.json();
+    renderProductsList(products);
+  } catch (error) {
+    alert('Failed to load products');
+  } finally {
+    loadingComponent.innerText = '';
+  }
 };
 
 renderProducts();
