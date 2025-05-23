f0cf9692dc966ba7ead4dce6dd4a3067cc1bdfb6
TasinLunatic <ahmedtasin207@gmail.com>
2025-05-23 20:40:40 +0800

    decrement added

---
+++ 001-counter-app/TasinLM10/script.js
@@ -0,0 +1,7 @@
+const decrementButton =document.getElementById('decrement');
+
+function decrement(){
+    alert("decrement button has been clicked");
+}
+
+decrementButton.addEventListener("click",decrement);
