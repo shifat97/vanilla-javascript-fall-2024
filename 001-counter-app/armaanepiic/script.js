decec4ed0df3ed1b518c4d7a0d15c345b1bde220
arman <armaanepiicfb17@gmail.com>
2025-06-24 13:40:40 +0600

    counter app created and applied css

--- 001-counter-app/armaanepiic/script.js
+++ 001-counter-app/armaanepiic/script.js
@@ -1,34 +1,33 @@
 let count = 0;
-    const counterValue = document.getElementById('counter');
-    const incrementBtn = document.getElementById('increment');
-    const decrementBtn = document.getElementById('decrement');
-    const reset = document.getElementById('reset');
+const counterValue = document.getElementById("counter");
+const incrementBtn = document.getElementById("increment");
+const decrementBtn = document.getElementById("decrement");
+const reset = document.getElementById("reset");
 
-    function handleIncrement(){
-        if(count + 1 == 11){
-            alert("Counter can not greater than 10");
-            count = 0;
-            counterValue.innerText = count;
-            return;
-        }
-        count++;
-        counterValue.innerText = count;
-    }
-    function handleDecrement(){
-        if(count > 0){
-            count--;
-            counterValue.innerText = count;
-        }
-        else{
-            alert("invalid");
-            return;
-        }
-    }
-    function handleReset(){
-        count = 0;
-        counterValue.innerText = count;
-    }
+function handleIncrement() {
+  if (count + 1 == 11) {
+    alert("Counter can not greater than 10");
+    count = 0;
+    counterValue.innerText = count;
+    return;
+  }
+  count++;
+  counterValue.innerText = count;
+}
+function handleDecrement() {
+  if (count > 0) {
+    count--;
+    counterValue.innerText = count;
+  } else {
+    alert("invalid");
+    return;
+  }
+}
+function handleReset() {
+  count = 0;
+  counterValue.innerText = count;
+}
 
-    incrementBtn.addEventListener('click', handleIncrement);
-    decrementBtn.addEventListener('click', handleDecrement);
-    reset.addEventListener('click', handleReset);
+incrementBtn.addEventListener("click", handleIncrement);
+decrementBtn.addEventListener("click", handleDecrement);
+reset.addEventListener("click", handleReset);
